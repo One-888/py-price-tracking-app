57d611a6c0ae27ab4bd374f59a8c31ef816356ed
One-888 <53633937+One-888@users.noreply.github.com>
2022-10-10 13:47:46 -0400

    Remove a global variable

--- price_tracking.py
+++ price_tracking.py
@@ -6,27 +6,35 @@ import keyboard as kb
 
 import ocr_unit_test as ocr
 
-price = []
 
-
-def get_file_list():
+def get_price_from_file():
+    price = []
     file_list = glob.glob(r"*.png")
     for file in file_list:
         # print(f"{file}")
         pic_to_float = ocr.ocr_process_single_value(f"{file}")
         # print(f"{pic_to_float}")
         price.append(pic_to_float)
+    return price
 
 
-# while True:
-# kb.press_and_release("shift+print_screen")
-# time.sleep(600)
+def display_result():
+    p1 = get_price_from_file()
+    p1 = set(p1)
+    p1 = list(p1)
+    p1.sort()
+    return p1
 
 
-get_file_list()
-price = set(price)
-price = list(price)
-price.sort()
+def main():
+    while True:
+        # kb.press_and_release("shift+print_screen")
+        p2 = display_result()
+        print(f"Min: {str(p2[0]).rjust(10, ' ')}")
+        print(f"Max: {str(p2[-1]).rjust(10, ' ')}")
+        pause_time_second = 300
+        time.sleep(pause_time_second)
+        print("Wait {pause_time_second} sec")
 
-print(f"Min: {str(price[0]).rjust(10, ' ')}")
-print(f"Max: {str(price[-1]).rjust(10, ' ')}")
+
+main()
